7bdbfda6c19059fdf367cc3af84a3b53c07f3eac
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-22 18:02:15 +0100

    add CocktailController for API

--- config/cocktails.php
+++ config/cocktails.php
@@ -1,15 +1,13 @@
-<?php 
+<?php
 
 $cocktails = [];
 
 
 //loop for get cocktails
-for ($i=0; $i < 3; $i++) { 
+for ($i = 0; $i < 3; $i++) {
     $cocktail_url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
     $coctailContent = file_get_contents($cocktail_url);
     $cocktailEncode = json_decode($coctailContent, true);
     array_push($cocktails, $cocktailEncode);
 }
-dd($cocktails);
-
-?>
+//dd($cocktails);
